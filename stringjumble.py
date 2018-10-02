06aa96aa2d1ea1b7fc3e02e8f849e00262f84af4
mgardner3901 <31485789+mgardner3901@users.noreply.github.com>
2018-10-01 21:03:04 -0400

    Updated from Brython Server: 10/1/2018 9:03:04 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,3 +25,43 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+
+userInput = input("Please enter a string of text (the bigger the better): ")
+
+print("You entered " +'"' + userInput +'"'+ ". Now jumble it:")
+
+word= ""
+words = []
+wordsBackwards=[]
+
+for s in userInput:
+        if s == " ":
+            words.append(word)
+            word=""
+        else:
+            word = word + s
+              
+words.append(word)
+
+
+
+for i in range(len(words)-1, -1, -1):
+    wordsBackwards.append(words[i])
+
+rev= "" 
+for s in userInput:
+    rev = s + rev
+            
+print(rev)
+
+print(' '.join(wordsBackwards))
+
+characterRev = words
+
+for t in range(0, len(characterRev), 1):
+    wordRev = ""
+    for r in (characterRev[t]):
+        wordRev= r + wordRev
+    characterRev[t] = wordRev
+
+print(' '.join(characterRev))
